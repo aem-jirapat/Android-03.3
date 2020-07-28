c51874efa36d88c0c20b0628f7819ac6cb895d71
aem-jirapat <aemjirapat.tr@gmail.com>
2020-07-28 17:27:44 +0700

    AddSetHasOptionsMenu

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -39,6 +39,7 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
